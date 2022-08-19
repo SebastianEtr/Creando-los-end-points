ac4c8e2a143a1bf00a6fdd97bc35b4011dd8e814
sebaseter <sebaslopezz654@gmail.com>
2022-08-18 22:56:18 -0500

    arreglo del httpCode

--- src/sword/sword.controller.ts
+++ src/sword/sword.controller.ts
@@ -1,24 +1,28 @@
-import { Controller, Get, Delete, Post, Put } from '@nestjs/common';
+import { Controller, Get, Delete, Post, Put, HttpCode } from '@nestjs/common';
 
 @Controller('sword')
 export class SwordController {
 
  @Get()
+ @HttpCode(202)
   getSword(): {} {
     return  {nombre: "Get"}
 }
 
 @Post()
+@HttpCode(202)
   CreateSword(): {} {
     return {nombre: "Post"}
 }
 
 @Delete()
+@HttpCode(202)
   DeleteSword(): {} {
     return  {nombre: "Delete"}
 }
 
 @Put()
+@HttpCode(202)
   AñadirSword(): {} {
     return  {nombre: "Get"} 
 }
